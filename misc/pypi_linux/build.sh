f89a58aa430ae3de6049f8d038c620755a962785
Isaiah Norton <isaiah.norton@gmail.com>
2019-04-05 17:17:17 -0400

    Working version w/ instructions for py27 and py37

---
+++ misc/pypi_linux/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# USAGE
+#------
+# 1) docker build .
+# - copy resulting IMAGE_HASH 
+# 2) docker run -v wheels:/wheels -ti IMAGE_HASH build.sh
+
+set -ex
+
+# build python27 wheel
+cd /home/tiledb
+git clone https://github.com/TileDB-Inc/TileDB-Py
+git -C TileDB-Py checkout $TILEDB_PY_VERSION
+
+cd /home/tiledb/TileDB-Py
+/opt/python/cp27-cp27mu/bin/python2.7 setup.py build_ext bdist_wheel --tiledb=/usr/local
+auditwheel repair dist/*.whl
+
+# build python37 wheel
+cd /home/tiledb
+git clone https://github.com/TileDB-Inc/TileDB-Py TileDB-Py3
+git -C TileDB-Py3 checkout $TILEDB_PY_VERSION
+
+cd /home/tiledb/TileDB-Py3
+/opt/python/cp37-cp37m/bin/python3.7 setup.py build_ext bdist_wheel --tiledb=/usr/local
+auditwheel repair dist/*.whl
+
+# copy build products out
+cp /home/tiledb/TileDB-Py/wheelhouse/* /wheels && \
+cp /home/tiledb/TileDB-Py3/wheelhouse/* /wheels
+
