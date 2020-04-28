dbe7b38306b893eeb935aae52ad4932d0ab0b1ee
Isaiah Norton <isaiah@tiledb.io>
2020-04-28 14:54:42 -0400

    add pycall debugging helper

--- tiledb/core.cc
+++ tiledb/core.cc
@@ -33,13 +33,60 @@ query requires
    not sure if size estimation works -- it should)
 */
 
+
+/* DEBUGGING CODE */
 #if !defined(NDEBUG)
+#include <pybind11/embed.h>
+
+namespace {
 extern "C" {
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
 __attribute__((used)) static void pyprint(pybind11::object o) {
   pybind11::print(o);
 }
+
+__attribute__((used)) static void pycall1(const char *expr,
+                                          pybind11::object o = py::none()) {
+  // this doesn't work in lldb
+  //py::scoped_interpreter guard{};
+
+  /*
+   * NOTE: the catch statements below do not work in lldb, because exceptions
+   *       are trapped internally. So, an error in eval currently breaks
+   *       use of this function until the process is restarted.
+   */
+
+  py::object res = py::none();
+  try {
+    if (!o.is(py::none())) {
+      auto locals = py::dict("_v"_a = o);
+      res = py::eval(expr, py::globals(), locals);
+    } else {
+      res = py::eval(expr, py::globals());
+    }
+    if (!res.is(py::none())) {
+      py::print(res);
+    }
+  } catch (py::error_already_set &e) {
+    std::cout << "pycall error_already_set: " << std::endl;
+  } catch (std::runtime_error &e) {
+    std::cout << "pycall runtime_error: " << e.what() << std::endl;
+  } catch (...) {
+    std::cout << "pycall unknown exception" << std::endl;
+  }
 }
+
+__attribute__((used)) static void pycall(const char *expr) {
+  pycall1(expr, py::none());
+}
+}
+};
 #endif
+
+/* END DEBUGGING CODE */
 
 namespace tiledbpy {
 
